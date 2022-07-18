9727bda58a315442d0bef29502ed83116d7281fe
mohitRana-04 <mohitrana04032003@gmail.com>
2022-07-18 09:47:00 +0530

    added sass and know

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./Square.style.css"
 
 function Square({value}) {
   // console.log(props)
   return (
-    <button type='button'>{value}</button>
+    <button type='button' className='square' >{value}</button>
   )
 }
 
