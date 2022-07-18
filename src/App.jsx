8ded4ea8cca1f7d18c1228ac99ed9a684b309cd0
mohitRana-04 <mohitrana04032003@gmail.com>
2022-07-18 07:38:18 +0530

    files changed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => (
+  <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <h3>Hi its me Mohit</h3>
+  </>
+);
+
+export default App;
